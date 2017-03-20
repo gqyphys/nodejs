46708bacd5dcc3948f0750e1e59f57edb60a925e
guqiuyue <gqyphy911@163.com>
2017-03-20 22:37:10 +0800

    a

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-// var fs = require("fs");
-//
-//
-// fs.readFile("input.txt", function (err, data) {
-//     if(err) return console.error(err);
-//     console.log(data.toString());
-// });
-//
-// console.log("程序执行结束！");
+var fs = require("fs");
+
+
+fs.readFile("input.txt", function (err, data) {
+    if(err) return console.error(err);
+    console.log(data.toString());
+});
+
+console.log("程序执行结束！");
 
 var events = require("events");
 var eventEmitter = new events.EventEmitter();

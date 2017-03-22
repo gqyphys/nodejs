4595f4a0eb617988450738fe8a1cf4dd764692b3
guqiuyue <gqyphy911@163.com>
2017-03-22 11:04:56 +0800

    a

--- buffer.js
+++ buffer.js
@@ -3,3 +3,8 @@ var buffer = new Buffer(256);
 var len = buffer.write("www.kaoshixing.com");
 
 console.log("write length:" + len);
+
+console.log(buffer.toString('ascii'));
+console.log(buffer.toString('ascii',0,3));
+console.log(buffer.toString('utf8',0,4));
+console.log(buffer.toString(undefined,0,5));

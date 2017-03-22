851e05b81ea270ac7c640062d63d6aa37a70e5ff
guqiuyue <gqyphy911@163.com>
2017-03-22 11:25:57 +0800

    a

--- buffer.js
+++ buffer.js
@@ -1,6 +1,7 @@
 var buff1 = new Buffer("www.");
-var buff2 = new Buffer("kaoshixing.com");
-var buff3 = Buffer.concat([buff1,buff2]);
+var buff2 = new Buffer("kaoshixing.");
+var buff3 = new Buffer("com");
+var buff4 = Buffer.concat([buff1,buff2,buff3]);
 
 
-console.log("write:" + buff3);
+console.log("write:" + buff4);

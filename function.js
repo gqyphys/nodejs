3396724704d1b9380fc60dc7c902a4b338aeb195
guqiuyue <gqyphy911@163.com>
2017-03-22 17:39:59 +0800

    a

--- function.js
+++ function.js
@@ -1,6 +1,6 @@
 var http = require("http");
 
-http.createSever(function(request, response) {
+http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();

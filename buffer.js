18e524eca30325f306eaa0f0e93260d032a7e621
guqiuyue <gqyphy911@163.com>
2017-03-22 11:07:14 +0800

    a

--- buffer.js
+++ buffer.js
@@ -8,3 +8,5 @@ console.log(buffer.toString('ascii'));
 console.log(buffer.toString('ascii',0,3));
 console.log(buffer.toString('utf8',0,4));
 console.log(buffer.toString(undefined,0,5));
+
+console.log(buffer.toJSON(buffer));

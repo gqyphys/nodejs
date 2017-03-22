fd7cb22a0f34909f26863b12cbf9b6524285270a
guqiuyue <gqyphy911@163.com>
2017-03-22 11:35:47 +0800

    a

--- buffer.js
+++ buffer.js
@@ -1,7 +1,7 @@
 var buff1 = new Buffer("www.");
 var buff2 = new Buffer("kaoshixing.");
 var buff3 = new Buffer("com");
-var buff4 = Buffer.concat([buff1,buff2,buff3]);
+var result = buff1.compare(buff2);
 
 
-console.log("write:" + buff4);
+console.log(result);

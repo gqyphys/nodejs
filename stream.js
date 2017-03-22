ca7bb6bc6dcad9b1e0cf1cfd0323b3b3a667b103
guqiuyue <gqyphy911@163.com>
2017-03-22 15:02:58 +0800

    a

--- stream.js
+++ stream.js
@@ -17,3 +17,20 @@ readStream.on("error", function() {
 });
 
 console.log("程序执行完毕");
+
+var data2 = "This is a output file.";
+
+var writeStream = fs.createWriteStream("output.txt");
+
+writeStream.write(data,"UTF8");
+writeStream.end();
+
+writeStream.on("finish", function() {
+    console.log("write finished");
+});
+
+writeStream.on("error", function() {
+    console.log(err.stack);
+});
+
+console.log("congratrulations!");

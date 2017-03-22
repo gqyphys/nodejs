d024610b81aa8f921d52b35c9c67776eb69fdfbd
guqiuyue <gqyphy911@163.com>
2017-03-22 15:34:26 +0800

    a

--- stream.js
+++ stream.js
@@ -1,39 +1,8 @@
 var fs = require("fs");
 
-// readStream
-var data = "";
-
 var readStream = fs.createReadStream("input.txt");
-readStream.setEncoding("UTF8");
-
-readStream.on("data", function(chunk) {
-    data += chunk;
-});
-
-readStream.on("end", function() {
-    console.log(data);
-});
-
-readStream.on("error", function() {
-    console.log(err.stack);
-});
-
-console.log("程序执行完毕");
-
-// writeStream
-var data2 = "This is a output file.";
-
 var writeStream = fs.createWriteStream("output.txt");
 
-writeStream.write(data2,"UTF8");
-writeStream.end();
+readStream.pipe(writeStream);
 
-writeStream.on("finish", function() {
-    console.log("write finished");
-});
-
-writeStream.on("error", function() {
-    console.log(err.stack);
-});
-
-console.log("congratrulations!");
+console.log("程序执行完毕");

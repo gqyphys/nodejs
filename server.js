462a391f25924049eb1ee9ee17ba09df5dcad22e
guqiuyue <gqyphy911@163.com>
2017-03-22 17:58:15 +0800

    a

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-var http = require('http');
+var http = require("http");
+var url = require("url");
 
-http.createServer(function (request, response) {
+function start() {
+	function onRequest(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
+		response.writeHead(200, {"Content-Type": "text/plain"});
+	    response.write("Hello World");
+	    response.end();
+	}
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started.");
+}
 
-	// 发送 HTTP 头部
-	// HTTP 状态值: 200 : OK
-	// 内容类型: text/plain
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-
-	// 发送响应数据 "Hello World"
-	response.end('Hello World\n');
-}).listen(8888);
-
-// 终端打印如下信息
-console.log('Server running at http://127.0.0.1:8888/');
+exports.start = start;
